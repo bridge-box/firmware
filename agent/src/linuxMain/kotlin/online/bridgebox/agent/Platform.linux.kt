70121ff06dc3f596fa9281843892c84e12def317
rcd27 <redtom@yandex.ru>
2026-04-02 13:06:10 +0300

    chore(clean-up): убран Kotlin/Native в качестве bb-agent

--- agent/src/linuxMain/kotlin/online/bridgebox/agent/Platform.linux.kt
+++
@@ -1,8 +0,0 @@
-package online.bridgebox.agent
-
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.toKString
-
-@OptIn(ExperimentalForeignApi::class)
-actual fun platformEnv(name: String): String? =
-    platform.posix.getenv(name)?.toKString()
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "bridgebox-firmware"
-
-include(":agent")
